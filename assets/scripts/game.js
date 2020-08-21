077129094a962fae0953e54008aa07422713b2fc
April Ha <aprilha3097@gmail.com>
2020-08-21 11:16:24 +0100

    Declared inital variables and added basic onclick function for buttons

---
+++ assets/scripts/game.js
@@ -0,0 +1,26 @@
+//declare the initial variables
+let btnReset = document.getElementsByClassName("btnReset");
+let btnHit = document.getElementsByClassName("btnHit");
+let btnStay = document.getElementsByClassName("btnStay");
+let winsDealer = document.getElementsByClassName("dealer-wins");
+let winsPlayer = document.getElementsByClassName("player-wins");
+
+let cardSuits = ["Hearts", "Clubs", "Diamonds", "Spades"];
+let cardValues = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven",
+"Six", "Five", "Four", "Three", "Two", "One"];
+
+
+
+
+$(".btnReset").click(function() {
+
+})
+
+$(".btnHit").click(function() {
+
+})
+
+$(".btnStay").click(function() {
+
+})
+
