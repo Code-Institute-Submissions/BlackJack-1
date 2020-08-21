f03d18e86d5c88f22d319389665a72c628346656
April Ha <aprilha3097@gmail.com>
2020-08-21 12:05:24 +0100

    Add title screen prior to starting game with start and how to play buttons

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,7 +1,7 @@
 //declare the initial variables
-let btnReset = document.getElementsByClassName("btnReset");
-let btnHit = document.getElementsByClassName("btnHit");
-let btnStay = document.getElementsByClassName("btnStay");
+let btnReset = document.getElementById("btnReset");
+let btnHit = document.getElementById("btnHit");
+let btnStay = document.getElementById("btnStay");
 let winsDealer = document.getElementsByClassName("dealer-wins");
 let winsPlayer = document.getElementsByClassName("player-wins");
 
@@ -9,18 +9,23 @@ let cardSuits = ["Hearts", "Clubs", "Diamonds", "Spades"];
 let cardValues = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven",
 "Six", "Five", "Four", "Three", "Two", "One"];
 
+function titleScreen() {
+    document.getElementById('game-container').style.display = 'none';
+    
+};
+
+titleScreen();
 
 
-
-$(".btnReset").click(function() {
+$("#btnReset").click(function() {
 
 })
 
-$(".btnHit").click(function() {
+$("#btnHit").click(function() {
 
 })
 
-$(".btnStay").click(function() {
+$("#btnStay").click(function() {
 
 })
 
