85a47436ca371273905dfdbc3132db341cbb0e60
April Ha <aprilha3097@gmail.com>
2020-08-30 23:14:35 +0100

    Add modal box for instructions to pop up onclick of How to Play button

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -4,6 +4,7 @@
 let headerText = document.getElementById("header-text");
 let btnStartGame = document.getElementById("btnStart")
 let btnGameInstruct = document.getElementById("btnInstruct");
+let titleContainer = document.getElementById("title-container");
 
 //main game varaiables
 let mainGame = document.getElementById("game-container");
@@ -17,6 +18,8 @@ let stayButton = document.getElementById("btnStay");
 //table variables 
 let scoreTable = document.getElementById("score-table");
 
+let instructionsPopUp = document.getElementById("instructions-box");
+
 //define card values 
 let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 let values = ['Ace', 'King', 'Queen', 'Jack',
@@ -29,3 +32,39 @@ mainGame.style.display = 'none';
 controls.style.display = 'none';
 scoreTable.style.display = 'none';
 
+//reset game properties
+let startGame = false;
+let gameOver = false; 
+let playerWon = false;
+let dealerCards = [];
+let playerCards = [];
+let dealerScore = 0;
+let playerScore = 0;
+
+btnStartGame.addEventListener('click', function() {
+    titleContainer.style.display = 'none';
+
+})
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btnGameInstruct.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
