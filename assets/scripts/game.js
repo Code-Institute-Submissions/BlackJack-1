c7c3ac291e183b6abf495e4a3ffa538384c08e6d
April Ha <aprilha3097@gmail.com>
2020-08-30 23:43:00 +0100

    Created function to define the cards numerical values

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -96,3 +96,18 @@ function createNewDeck () {
     return deck;
 }
 
+//defining the card numerical values
+function cardValues(card) {
+    switch(card.value){
+        case 'Ace': return 1;
+        case 'Two': return 2;
+        case 'Three': return 3;
+        case 'Four': return 4;
+        case 'Five': return 5;
+        case 'Six': return 6;
+        case 'Seven': return 7;
+        case 'Eight': return 8;
+        case 'Nine': return 9;
+        defult: return 10;
+    }
+}
