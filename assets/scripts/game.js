c180c884d8d123ab3e5ffb3833c254a63b0d7540
April Ha <aprilha3097@gmail.com>
2020-08-30 22:18:13 +0100

    Refactor basic title screen structure and link game.js to index.html

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,26 +1,3 @@
-//declare the initial variables
-let btnDeal = document.getElementById("btnDeal");
-let btnHit = document.getElementById("btnHit");
-let btnStay = document.getElementById("btnStay");
-let winsDealer = document.getElementsByClassName("dealer-wins");
-let winsPlayer = document.getElementsByClassName("player-wins");
+// Define variables
 
-let cardSuits = ["Hearts", "Clubs", "Diamonds", "Spades"];
-let cardValues = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven",
-"Six", "Five", "Four", "Three", "Two", "One"];
-
-function titleScreen() {
-    document.getElementById('game-container').style.display = 'none';
-    
-}; 
-
-function mainGame() {
-    document.getElementById("title-container").style.display = 'none';
-}
-
-function instructions() {
-    document.getElementById("instructions").style.display = "none";
-}
-
-instructions();
-mainGame();
+let headerText = document.getElementById("header-text");
