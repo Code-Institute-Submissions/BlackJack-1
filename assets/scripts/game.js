7142ed85c5ef3a777c98c4ce0bec24c3ab2abf71
April Ha <aprilha3097@gmail.com>
2020-08-31 17:50:24 +0100

    Refactored game.js code

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,32 +1,11 @@
-// Define variables
-
-let userWinScore = 0;
-let userLoseScore = 0;
-let userDrawScore = 0;
-let userWins = document.getElementById("wins");
-let userLosses = document.getElementById("losses");
-let userDraw = document.getElementById("draw")
-
-//title screen variables
-let headerText = document.getElementById("header-text");
-let btnStartGame = document.getElementById("btnStart")
-let btnGameInstruct = document.getElementById("btnInstruct");
-let titleContainer = document.getElementById("title-container");
-
-//main game varaiables
+//Define varaiables 
 let mainGame = document.getElementById("game-container");
-
-//control buttons variables
-let controls = document.getElementById("controls");
-let dealButton = document.getElementById("btnDeal");
-let hitButton = document.getElementById("btnHit");
-let stayButton = document.getElementById("btnStay");
-
-//table variables 
-let scoreTable = document.getElementById("score-table");
-
-let instructionsPopUp = document.getElementById("instructions-box");
 let footer = document.getElementById('footer');
+let btnStartGame = document.getElementById("btnStart")
+let titleContainer = document.getElementById("title-container");
+let btnGameInstruct = document.getElementById("btnInstruct");
+let modal = document.getElementById("myModal");
+let span = document.getElementsByClassName("close")[0];
 
 //define card values 
 let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
@@ -39,35 +18,12 @@ let values = ['Ace', 'King', 'Queen', 'Jack',
 mainGame.style.display = 'none';
 footer.style.display = 'none';
 
-
-//titleContainer.style.display = 'none';
-
-//reset game properties
-let startGame = false;
-let gameOver = false; 
-let playerWon = false;
-let dealerCards = [];
-let playerCards = [];
-let dealerScore = 0;
-let playerScore = 0;
-
 //When the Start Game button is clicked this will take user to main game page
 btnStartGame.addEventListener('click', function() {
     titleContainer.style.display = 'none';
     mainGame.style.display = 'block';
     footer.style.display = 'block';
 })
-
-//When the Deal button is clicked, this will initalise the game
-dealButton.addEventListener('click', function() {
-    deck = creatNewDeck();
-})
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btnGameInstruct.onclick = function() {
@@ -107,48 +63,7 @@ function createNewDeck () {
     return deck;
 }
 
-console.log(createNewDeck());
 
-//defining the card numerical values
-function cardValues(card) {
-    switch(card.value){
-        case 'Ace': return 1;
-        case 'Two': return 2;
-        case 'Three': return 3;
-        case 'Four': return 4;
-        case 'Five': return 5;
-        case 'Six': return 6;
-        case 'Seven': return 7;
-        case 'Eight': return 8;
-        case 'Nine': return 9;
-        defult: return 10;
-    }
-}
-
-//Function for when user wins
-function win() {
-    //user score will go up by one when user wins
-    userWinScore++;
-    //change the HTML text to reflect new userscore
-    userWins.innerHTML = userWinScore;
-}
-
-
-//Function for when user losses
-function lose() {
-    //user losses score will go up by one when user losses
-    userLoseScore++;
-    //change the HTML text to reflect new userscore
-    userLosses.innerHTML = userLoseScore;
-}
-
-//Function for when user and dealer draw
-function draw(){
-    //draw score will go up by one when score is tied
-    userDrawScore++;
-    //change the HTML to reflect the new draw score
-    userDraw.innerHTML = userDrawScore;
-}
 
 
 
@@ -163,8 +78,7 @@ function draw(){
 
 
 //ON CLICK FUNCTION - when dealButton is clicked
+//A deck will be created
+//the deck will be shuffled
 //The game will begin - a pair of cards will be dealt to player and dealer 
 //Both HIt and Stay buttons will be active 
-
-
-//ON CLICK FUNCTION - when stayButton is clicked 
