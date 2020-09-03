50711e9ec40e6ed5994399727f1a20115a8d2f4b
April Ha <aprilha3097@gmail.com>
2020-09-03 12:37:53 +0100

    Hit button will now generate a randomCard index from the cardValues array which will be the gameCard

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,53 +1,44 @@
 
-//define card values and mapped values to correct numberical values
-//removed suits values as they were not needed in order for game logic to work
-//easier to work through the logic of the game
-let cards = {
-  'cardValues': ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two'],
-  'cardMapping': {'Ace': [1, 11], 'King': 10, 'Queen': 10, 'Jack': 10, 'Ten': 10, 'Nine': 9, 'Eight': 8, 'Seven': 7, 'Six': 6, 'Five': 5, 'Four': 4, 'Three': 3, 'Two': 2},
-};
+//define the cardValues in an array
+const cardValues = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two']
+
 
 //query select the control buttons
-document.querySelector('#btnHit').addEventListener('click', hitButton())
-document.querySelector('#btnStay').addEventListener('click', stayButton());
-document.querySelector("#btnDeal").addEventListener('click', dealButton());
+document.querySelector('#btnHit').addEventListener('click', hitButton);
+document.querySelector('#btnStay').addEventListener('click', stayButton);
+document.querySelector("#btnDeal").addEventListener('click', dealButton);
 
+function hitButton() {
+  let gameCard = randomCard();
+};
 
-dealButton.addEventListener('click', function() {
-  
-
-})
-
-hitButton.addEventListener('click', function() {
-
-
-})
-
-function shuffleDeck(deck) {
-  for(let i = 0; i < deck.length; i++) {
-    let randomChoice = Math.floor(Math.random() * 52);
-    let tmp = deck[randomChoice];
-    deck[randomChoice] = deck[i];
-    deck[i] = tmp;
-  }
-}
-
-//Define varaiables 
-let mainGame = document.getElementById("game-container");
-let footer = document.getElementById('footer');
-let btnStartGame = document.getElementById("btnStart")
-let titleContainer = document.getElementById("title-container");
-let btnGameInstruct = document.getElementById("btnInstruct");
-let modal = document.getElementById("myModal");
-let span = document.getElementsByClassName("close")[0];
+//Function to randomise the values
+function randomCard() {
+  const randomIndex = Math.floor(Math.random() * 13);
+  return cardValues[randomIndex];
+};
 
 
 
-// hidden elements
-mainGame.style.display = 'none';
-footer.style.display = 'none';
+/*
+function getCardValue() {
+  if(card === 'Ace') return 1;
+  if(card === 'King') return 10;
+  if(card === 'Queen') return 10;
+  if(card === 'Jack') return 10;
+  if(card === 'Ten') return 10;
+  if(card === 'Nine') return 9;
+  if(card === 'Eight') return 8;
+  if(card === 'Seven') return 7;
+  if(card === 'Six') return 6;
+  if(card === 'Five') return 5;
+  if(card === 'Four') return 4;
+  if(card === 'Three') return 3;
+  if(card === 'Two') return 2;
+};
+*/
 
-
+/*
 // When the user clicks on the button, open the modal
 btnGameInstruct.onclick = function() {
   modal.style.display = "block";
@@ -65,12 +56,11 @@ window.onclick = function(event) {
   }
 }
 
-
 //When the Start Game button is clicked this will take user to main game page
 btnStartGame.addEventListener('click', function() {
     titleContainer.style.display = 'none';
     mainGame.style.display = 'block';
     footer.style.display = 'block';
 });
-
+*/
 
