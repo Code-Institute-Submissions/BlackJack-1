c40393187d1d73f6390d78bef87aa73b8cb492b8
April Ha <aprilha3097@gmail.com>
2020-09-03 17:17:41 +0100

    Basic function added for dealButton to clear board and revert game back to the start for another round

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -2,15 +2,28 @@
 //define the cardValues in an array
 const cardValues = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two']
 
+let playerWins = [];
+let playerLosses = [];
+let playerDraws = [];
+
+let playersScore = [];
+let dealersScore = [];
+
+
 
 //query select the control buttons
 document.querySelector('#btnHit').addEventListener('click', hitButton);
-document.querySelector('#btnStay').addEventListener('click', stayButton);
+//document.querySelector('#btnStay').addEventListener('click', stayButton);
 document.querySelector("#btnDeal").addEventListener('click', dealButton);
 
 function hitButton() {
+  //a random card is generated
   let gameCard = randomCard();
+  //gameCardImage will take the random card and it will show the cards image
   gameCardImage(gameCard);
+  //update the total score 
+
+
 };
 
 //Function to randomise the values
@@ -28,25 +41,53 @@ function gameCardImage(gameCard) {
   document.querySelector('#players-box').appendChild(cardImage);
 }
 
+//When player presses the dealButton
+function dealButton() {
+  //define variables to select all images within the players and dealers boxes
+  let playersCardImages = document.querySelector("#players-box").querySelectorAll('img');
+  let dealersCardImages = document.querySelector("#dealers-box").querySelectorAll('img');
+
+  //for loop to remove all of the images within the boxes
+  //remove the images within the players box
+  for(i = 0; i < playersCardImages.length; i++){
+    playersCardImages[i].remove();
+  }
+//remove the images within the dealers box
+  for(j = 0; j < dealersCardImages.length; j++){
+    dealersCardImages[j].remove();
+  }
+
+  //player and dealers score are set back to be 0
+  playersScore = 0;
+  dealersScore = 0;
+
+  //players and dealers score spans will be set back to 0
+  document.querySelector("#players-result").innerHTML = 0;
+  document.querySelector("#dealers-result").innerHTML = 0;
+
+  //the status of the game will revert back to "lets play" after a winner or loser is indicated
+  document.querySelector("#blackjack-result").innerHTML = "Test test";
+
+}
 
 /*
-function getCardValue() {
-  if(card === 'Ace') return 1;
-  if(card === 'King') return 10;
-  if(card === 'Queen') return 10;
-  if(card === 'Jack') return 10;
-  if(card === 'Ten') return 10;
-  if(card === 'Nine') return 9;
-  if(card === 'Eight') return 8;
-  if(card === 'Seven') return 7;
-  if(card === 'Six') return 6;
-  if(card === 'Five') return 5;
-  if(card === 'Four') return 4;
-  if(card === 'Three') return 3;
-  if(card === 'Two') return 2;
-};
-*/
+function getCardNumericValue(gameCard){
+  switch(gameCard){
+    case 'Ace': return 1;
+    case 'Two': return 2;
+    case 'Three': return 3;
+    case 'Four': return 4;
+    case 'Five': return 5;
+    case 'Six': return 6;
+    case 'Seven': return 7;
+    case 'Eight': return 8;
+    case 'Nine': return 9;
+    default:
+      return 10; 
+  }
+}
 
+*/
 /*
 // When the user clicks on the button, open the modal
 btnGameInstruct.onclick = function() {
