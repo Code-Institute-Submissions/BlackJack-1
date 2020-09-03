274b73bb5bc83196472cfaf4a69694dd27b98c9d
April Ha <aprilha3097@gmail.com>
2020-09-03 16:31:14 +0100

    gameCardImage function so the cardValue will be shown with the correct card image

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -10,6 +10,7 @@ document.querySelector("#btnDeal").addEventListener('click', dealButton);
 
 function hitButton() {
   let gameCard = randomCard();
+  gameCardImage(gameCard);
 };
 
 //Function to randomise the values
@@ -18,6 +19,14 @@ function randomCard() {
   return cardValues[randomIndex];
 };
 
+//Whenever the HIT button is clicked a card will be shown
+function gameCardImage(gameCard) {
+  //create a section within the player's box so the image can be shown within the box
+  let cardImage = document.createElement('img');
+  //the gameCard will match the name of the png and that card will show on the board
+  cardImage.src = `assets/images/PNG/${gameCard}.png`;
+  document.querySelector('#players-box').appendChild(cardImage);
+}
 
 
 /*
